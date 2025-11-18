568d3e348558eaf4a45b67324fbda45deb4594fd
Adnan <shaikhadnan030303@gmail.com>
2025-11-18 15:08:00 +0530

    Removed old folder Hostel_Project_Relational_Mapping_06_Oct

--- Hostel_Project_Relational_Mapping_06_Oct/src/main/java/com/example/demo/repository/OrganizationRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Organization;
-
-public interface OrganizationRepo extends JpaRepository<Organization, Integer> {
-
-}
